d55e24a1c8d62b3711075cccd4e692553cc752d1
Ad31ide <seanchendu@gmail.com>
2023-07-04 00:56:45 -0500

    trying to make stuff work

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+
 var password=document.getElementById("password")
 
 var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength = 12;
+var passwordLength = prompt('how long do you want your password to be?');
 var password = ""
 
-for (var i=0; i <= passwordLength; i++){
-    var randomNumber = Math.floor(Math.random()*chars.length);
-    password += chars.substring(randomNumber,randomNumber +1);
+function get_password(){
+    for(var i = 0; i <= passwordLength; i++){
+        var randomNumber = Math.floor(Math.random()* chars.length);
+        password += chars.substring(randomNumber,randomNumber +1);
+        console.log(password);
+    }
+    console.log(`fin ${password}`)
+    document.getElementById("jeb").innerHTML = "1234567";
 }
 
-document.getElementById('password').value = password;
+// function get_password(){
+//     for (var i=0; i <= passwordLength; i++){
+//         var randomNumber = Math.floor(Math.random()*chars.length);
+//         password += chars.substring(randomNumber,randomNumber +1);
+//     }
+//     document.getElementById('password').innerHTML= password;
+// }
+
+
+console.log(password)
 
 function copyPassword(){
     var copyText = document.getEleementById("password");
     copyText.select();
     document.execCommand('copy');
 }
+
---
+++ test/sub.js
@@ -0,0 +1,11 @@
+var num1 = prompt('give me a number man')
+var num2 = prompt('another number')
+
+function trySomethingOut(){
+    console.log("Yep, I called this function")
+    document.getElementById('frame').innerHTML("I hope this works") ;
+}
+
+document.getElementById('frame').innerHTML = " num1 + num2" ;
+document.write("I hate this place")
+
